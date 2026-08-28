5ff01dea3b78d7053975949bb38bdd0e9327d374
hmj2622003Sawada <hmj2622003@stu.o-hara.ac.jp>
2026-08-28 16:49:24 +0900

    背景、プレイヤーの画像の追加、画像に動きを追加、背景の表示

---
+++ Main.cpp
@@ -0,0 +1,110 @@
+#include "DxLib.h"
+#include <stdlib.h>
+#include "Mainheader.h"
+
+// 定数の定義
+const int WIDTH = 444, HEIGHT = 340; // 画面サイズ
+const int FPS = 60; // フレームレート
+enum{TITLE, RULE, PLAY, RESULT};
+
+int imgPlayer[6] = {
+	LoadGraph("image/run1.png"),
+	LoadGraph("image/run2.png"),
+	LoadGraph("image/run3.png"),
+	LoadGraph("image/run4.png"),
+	LoadGraph("image/run5.png"),
+	LoadGraph("image/run6.png")
+};
+int PlayerX = 0, PlayerY = 0;
+
+// グローバル変数
+int distance = 0;
+// int imgPlayer, imgCPU;
+int imgBG;
+int timer = 0;
+int scene = TITLE;
+
+
+
+int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
+{
+	SetWindowText("Nuku Game");	// ウィンドウのタイトル
+	SetGraphMode(WIDTH, HEIGHT, 32);	// ウィンドウの大きさとカラー指定
+	ChangeWindowMode(TRUE);	// ウィンドウモードで起動
+	if (DxLib_Init() == -1) return -1;	// ライブラリの初期化、エラーが起きたら終了
+	SetBackgroundColor(0, 0, 0);	// 背景色の指定
+	SetDrawScreen(DX_SCREEN_BACK);	// 描画面を裏画面に
+
+	InitGame(); // 初期化用の関数
+
+
+	int imgPlayer[6] = {
+	LoadGraph("image/run1.png"),
+	LoadGraph("image/run2.png"),
+	LoadGraph("image/run3.png"),
+	LoadGraph("image/run4.png"),
+	LoadGraph("image/run5.png"),
+	LoadGraph("image/run6.png")
+	};
+	int PlayerX = 0, PlayerY = 0;
+
+	while (1) // メインループ
+	{
+		ClearDrawScreen(); // 画面をクリアにする
+
+		DrawGraph(0, 0, imgBG, FALSE);// 背景表示
+		PlayerX = PlayerX + 5;
+		if (PlayerX > WIDTH) PlayerX = -100;
+		DrawGraph(PlayerX, PlayerY, imgPlayer[(timer/7) % 6], TRUE);
+		timer++;
+
+		ScreenFlip();	// 裏画面の内容を表画面に反映
+		WaitTimer(1000 / FPS);	// 一定時間待機
+		if (ProcessMessage() == -1) break;	// エラーが起きたら終了
+		if (CheckHitKey(KEY_INPUT_ESCAPE) == 1) break;	// Escが押されたら終了
+	}
+
+	DxLib_End(); // DXライブラリの終了
+	return 0;	// 終了
+}
+
+
+// 自作の関数
+
+// 初期化用の関数
+void InitGame(void)
+{
+	// 背景、画像の読み込み 
+	imgBG = LoadGraphWithCheck("image/haikei.jpeg");
+	// プレイヤーの画像
+	
+	// サウンドの読み込みと音量設定
+//  bgm = LoadSoundMemWithCheck("sound/bgm.wav");
+
+}
+
+// 画像、音声の読み込み失敗通知
+int LoadGraphWithCheck(const char* file)
+{
+	int res = LoadGraph(file);
+	if (res == -1)
+	{
+		MessageBox(GetMainWindowHandle(), file, "画像読み込みに失敗", MB_OK | MB_ICONSTOP);
+	}
+	return res;
+}
+
+int LoadSoundMemWithCheck(const char* file)
+{
+	int res = LoadSoundMem(file);
+	if (res == -1)
+	{
+		MessageBox(GetMainWindowHandle(), file, "音声の読み込みに失敗", MB_OK | MB_LEN_MAX);
+		return res;
+	}
+}
+
+
+
+
+// 背景のサイズH444×W340
---
+++ Mainheader.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// \‘¢‘Ì‚ÌéŒ¾
+struct OBJECT
+{
+
+};
+
+// ŠÖ”éŒ¾
+void InitGame(void);
+int LoadGraphWithCheck(const char* file);
+int LoadSoundMemWithCheck(const char* file);
